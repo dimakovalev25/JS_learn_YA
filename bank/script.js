cbe588e5c1dd4b713b1d98b5f7a6d287d461684b
dimakovalev25 <d.nozdryn@gmail.com>
2023-01-20 21:10:29 +0300

    project bank start

--- bank/script.js
+++ bank/script.js
@@ -3,38 +3,38 @@
 // Simply Bank App
 
 const account1 = {
-  userName: 'Cecil Ireland',
-  transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
-  interest: 1.5,
-  pin: 1111,
+    userName: 'Cecil Ireland',
+    transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
+    interest: 1.5,
+    pin: 1111,
 };
 
 const account2 = {
-  userName: 'Amani Salt',
-  transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
-  interest: 1.3,
-  pin: 2222,
+    userName: 'Amani Salt',
+    transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
+    interest: 1.3,
+    pin: 2222,
 };
 
 const account3 = {
-  userName: 'Corey Martinez',
-  transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
-  interest: 0.8,
-  pin: 3333,
+    userName: 'Corey Martinez',
+    transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
+    interest: 0.8,
+    pin: 3333,
 };
 
 const account4 = {
-  userName: 'Kamile Searle',
-  transactions: [530, 1300, 500, 40, 190],
-  interest: 1,
-  pin: 4444,
+    userName: 'Kamile Searle',
+    transactions: [530, 1300, 500, 40, 190],
+    interest: 1,
+    pin: 4444,
 };
 
 const account5 = {
-  userName: 'Oliver Avila',
-  transactions: [630, 800, 300, 50, 120],
-  interest: 1.1,
-  pin: 5555,
+    userName: 'Oliver Avila',
+    transactions: [630, 800, 300, 50, 120],
+    interest: 1.1,
+    pin: 5555,
 };
 
 const accounts = [account1, account2, account3, account4, account5];
@@ -64,3 +64,29 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
+
+containerTransactions.innerHTML = '';
+
+const displayTransactions = function (transactions) {
+    transactions.forEach(function (trans, index) {
+
+        const transType = trans > 0 ? 'deposit' : 'withdrawal';
+
+        const transactionRow = `
+            <div class="transactions__row">
+                <div class="transactions__type transactions__type--${transType}">
+            ${index + 1} ${transType}
+            </div>
+                <div class="transactions__value">${trans}</div>
+            </div>`
+
+        containerTransactions.insertAdjacentHTML('afterbegin', transactionRow)
+
+    });
+}
+
+displayTransactions(account1.transactions)
+
+labelBalance.innerHTML = '';
+const balance = account1.transactions.reduce((acc, b) => acc +b, 0);
+labelBalance.innerHTML = `${balance} $`
