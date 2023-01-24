080b64bcfe425b9a984005ce970f36eadb536820
dimakovalev25 <d.nozdryn@gmail.com>
2023-01-24 22:24:07 +0300

    bank date done

--- bank/script.js
+++ bank/script.js
@@ -1,5 +1,7 @@
 'use strict';
 // Elements
+const logo = document.querySelector('.logo');
+
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
 const labelBalance = document.querySelector('.balance__value');
@@ -28,10 +30,22 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // Simply Bank App
 
 const account1 = {
-    userName: 'q',
+    userName: 'C',
     transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
-    interest: 1.15,
+    interest: 1.5,
     pin: 1,
+    transactionsDates: [
+        '2020-10-02T14:43:31.074Z',
+        '2020-10-29T11:24:19.761Z',
+        '2020-11-15T10:45:23.907Z',
+        '2021-01-22T12:17:46.255Z',
+        '2021-02-12T15:14:06.486Z',
+        '2021-03-09T11:42:26.371Z',
+        '2021-05-21T07:43:59.331Z',
+        '2021-06-22T15:21:20.814Z',
+    ],
+    currency: 'USD',
+    locale: 'en-US',
 };
 
 const account2 = {
@@ -39,6 +53,18 @@ const account2 = {
     transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
     interest: 1.3,
     pin: 2222,
+    transactionsDates: [
+        '2020-10-02T14:43:31.074Z',
+        '2020-10-29T11:24:19.761Z',
+        '2020-11-15T10:45:23.907Z',
+        '2021-01-22T12:17:46.255Z',
+        '2021-02-12T15:14:06.486Z',
+        '2021-03-09T11:42:26.371Z',
+        '2021-05-21T07:43:59.331Z',
+        '2021-06-22T15:21:20.814Z',
+    ],
+    currency: 'UAH',
+    locale: 'uk-UA',
 };
 
 const account3 = {
@@ -46,6 +72,18 @@ const account3 = {
     transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
     interest: 0.8,
     pin: 3333,
+    transactionsDates: [
+        '2020-10-02T14:43:31.074Z',
+        '2020-10-29T11:24:19.761Z',
+        '2020-11-15T10:45:23.907Z',
+        '2021-01-22T12:17:46.255Z',
+        '2021-02-12T15:14:06.486Z',
+        '2021-03-09T11:42:26.371Z',
+        '2021-05-21T07:43:59.331Z',
+        '2021-06-22T15:21:20.814Z',
+    ],
+    currency: 'RUB',
+    locale: 'ru-RU',
 };
 
 const account4 = {
@@ -53,6 +91,18 @@ const account4 = {
     transactions: [530, 1300, 500, 40, 190],
     interest: 1,
     pin: 4444,
+    transactionsDates: [
+        '2020-10-02T14:43:31.074Z',
+        '2020-10-29T11:24:19.761Z',
+        '2020-11-15T10:45:23.907Z',
+        '2021-01-22T12:17:46.255Z',
+        '2021-02-12T15:14:06.486Z',
+        '2021-03-09T11:42:26.371Z',
+        '2021-05-21T07:43:59.331Z',
+        '2021-06-22T15:21:20.814Z',
+    ],
+    currency: 'EUR',
+    locale: 'fr-CA',
 };
 
 const account5 = {
@@ -60,26 +110,53 @@ const account5 = {
     transactions: [630, 800, 300, 50, 120],
     interest: 1.1,
     pin: 5555,
+    transactionsDates: [
+        '2020-10-02T14:43:31.074Z',
+        '2020-10-29T11:24:19.761Z',
+        '2020-11-15T10:45:23.907Z',
+        '2021-01-22T12:17:46.255Z',
+        '2021-02-12T15:14:06.486Z',
+        '2021-03-09T11:42:26.371Z',
+        '2021-05-21T07:43:59.331Z',
+        '2021-06-22T15:21:20.814Z',
+    ],
+    currency: 'USD',
+    locale: 'en-US',
 };
 
 const accounts = [account1, account2, account3, account4, account5];
+
+const now = new Date();
+const day = `${now.getDay()}`.padStart(2, '0');
+const month = `${now.getMonth()+1}`.padStart(2,'0');
+const year = now.getFullYear();
+const transDate =`${day}/${month}/${year}`;
+labelDate.textContent = transDate;
+
 
 containerTransactions.innerHTML = '';
 let mainAccount;
 inputLoginUsername.value = 'Dima Kovalev';
 inputLoginPin.value = '2222';
 
-const displayTransactions = function (transactions) {
-    transactions.forEach(function (trans, index) {
+const displayTransactions = function (account) {
+    account.transactions.forEach(function (trans, index) {
 
         const transType = trans > 0 ? 'deposit' : 'withdrawal';
+
+        const date = new Date(account.transactionsDates[index]);
+        const day = `${date.getDay()}`.padStart(2, '0');
+        const month = `${date.getMonth()+1}`.padStart(2,'0');
+        const year = date.getFullYear();
+        const transDate =`${day}/${month}/${year}`;
 
         const transactionRow = `
             <div class="transactions__row">
                 <div class="transactions__type transactions__type--${transType}">
             ${index + 1} ${transType}
             </div>
-                <div class="transactions__value">${trans}</div>
+            <div class="transactions__date">${transDate}</div>
+                <div class="transactions__value">${trans.toFixed(2)}</div>
             </div>`
 
         containerTransactions.insertAdjacentHTML('afterbegin', transactionRow)
@@ -91,20 +168,20 @@ const displayBalance = function (mainAccount) {
     labelBalance.innerHTML = '';
     const balance = mainAccount.transactions.reduce((acc, b) => acc + b, 0);
     mainAccount.balance = balance;
-    labelBalance.innerHTML = `${balance} $`
+    labelBalance.innerHTML = `${balance.toFixed(2)} $`
     // console.log(mainAccount)
 
     labelSumIn.innerHTML = '';
     const balanceIn = mainAccount.transactions.filter(trans => trans > 0).reduce((acc, a) => acc + a, 0);
-    labelSumIn.innerHTML = balanceIn + '$';
+    labelSumIn.innerHTML = balanceIn.toFixed(2) + '$';
 
     labelSumInterest.innerHTML = '$';
     const interest = Math.floor(mainAccount.transactions.filter(trans => trans > 0).map(dep => dep * 1.15 / 100).reduce((acc, a) => acc + a, 0));
-    labelSumInterest.innerHTML = interest + '$';
+    labelSumInterest.innerHTML = interest.toFixed(2) + '$';
 
     labelSumOut.innerHTML = '';
     const balanceOut = mainAccount.transactions.filter(trans => trans < 0).reduce((acc, a) => acc + a, 0);
-    labelSumOut.innerHTML = balanceOut + '$';
+    labelSumOut.innerHTML = balanceOut.toFixed(2) + '$';
 }
 
 btnLogin.addEventListener('click', function (e) {
@@ -123,7 +200,7 @@ btnLogin.addEventListener('click', function (e) {
 
         labelWelcome.textContent = `Welcome, ${mainAccount.userName.split(' ')[0]}!`
 
-        displayTransactions(mainAccount.transactions);
+        displayTransactions(mainAccount);
         displayBalance(mainAccount)
 
 
@@ -142,28 +219,31 @@ btnTransfer.addEventListener('click', function (e) {
     // console.log(transferAmount, recepientAccount);
 
     if (transferAmount > 0 && mainAccount.balance >= transferAmount && recepientAccount && mainAccount.userName !== recepientAccount?.userName) {
-        // console.log('tr')
+
+
         mainAccount.transactions.push(-transferAmount);
         recepientAccount.transactions.push(transferAmount);
+        mainAccount.transactionsDates.push(new Date().toISOString());
+        recepientAccount.transactionsDates.push(new Date().toISOString());
 
-      displayTransactions(mainAccount.transactions);
-      displayBalance(mainAccount);
+        displayTransactions(mainAccount);
+        displayBalance(mainAccount);
 
 
     }
 });
 
-btnClose.addEventListener('click', function(e) {
+btnClose.addEventListener('click', function (e) {
     e.preventDefault();
     const user = inputCloseUsername.value;
     const pin = inputClosePin.value;
 
-    if(inputCloseUsername.value === mainAccount.userName && Number(inputClosePin.value) === mainAccount.pin) {
+    if (inputCloseUsername.value === mainAccount.userName && Number(inputClosePin.value) === mainAccount.pin) {
         const mainAccountIndex = accounts.findIndex(account => account.userName === inputCloseUsername.value)
         accounts.splice(mainAccountIndex, 1);
         containerApp.style.opacity = 0;
-        inputClosePin.value='';
-        inputCloseUsername.value='';
+        inputClosePin.value = '';
+        inputCloseUsername.value = '';
         labelWelcome.textContent = `Log into your account!`
         // console.log(accounts);
     }
@@ -174,10 +254,11 @@ btnLoan.addEventListener('click', function (e) {
     const loanAmount = Number(inputLoanAmount.value)
 
     console.log(inputLoanAmount.value);
-    if(loanAmount > 0 && mainAccount.transactions.some(tr => tr >= loanAmount/10 )){
+    if (loanAmount > 0 && mainAccount.transactions.some(tr => tr >= loanAmount / 10)) {
         mainAccount.transactions.push(loanAmount);
-        mainAccount.loan =loanAmount;
-        displayTransactions(mainAccount.transactions);
+        mainAccount.transactionsDates.push(new Date().toISOString());
+        mainAccount.loan = loanAmount;
+        displayTransactions(mainAccount);
         displayBalance(mainAccount);
         inputLoanAmount.value = '';
     }
@@ -185,8 +266,19 @@ btnLoan.addEventListener('click', function (e) {
 
 btnSort.addEventListener('click', function (e) {
     e.preventDefault();
-    containerTransactions.innerHTML=''
-    mainAccount.transactions.sort((a, b) => a-b);
-    displayTransactions(mainAccount.transactions);
+    containerTransactions.innerHTML = ''
+    mainAccount.transactions.sort((a, b) => a - b);
+    displayTransactions(mainAccount);
+
+})
+
+logo.addEventListener('click', function (e) {
+
+    [...document.querySelectorAll('.transactions__row')].forEach((row, i) => {
+        if(i % 2 === 0){
+            row.style.backgroundColor = '#f3f3f3';
+        }
+        // console.log(row, i)
+    })
 
 })
--- script.js
+++ script.js
@@ -1,2 +1,68 @@
 'use strict';
 
+const account1 = {
+    userName: 'q',
+    transactions: [500, 250, -300, 5000, -850, -110, -170, 1100],
+    interest: 1.15,
+    pin: 1,
+};
+
+const account2 = {
+    userName: 'Dima Kovalev',
+    transactions: [2000, 6400, -1350, -70, -210, -2000, 5500, -30],
+    interest: 1.3,
+    pin: 2222,
+};
+
+const account3 = {
+    userName: 'Corey Martinez',
+    transactions: [900, -200, 280, 300, -200, 150, 1400, -400],
+    interest: 0.8,
+    pin: 3333,
+};
+
+const account4 = {
+    userName: 'Kamile Searle',
+    transactions: [530, 1300, 500, 40, 190],
+    interest: 1,
+    pin: 4444,
+};
+
+const account5 = {
+    userName: 'Oliver Avila',
+    transactions: [630, 800, 300, 50, 120],
+    interest: 1.1,
+    pin: 5555,
+};
+
+const accounts = [account1, account2, account3, account4, account5];
+
+const total = accounts.map(a => a.transactions).flat().sort((a, x) => x - a).filter(a => a > 0).reduce((a, b) => a + b);
+
+const totalMap = accounts.flatMap(a => a.transactions).reduce((acc, a) => a > 0 ? acc + a : acc);
+
+const totalRed = accounts
+    .flatMap(a => a.transactions)
+    .reduce((acc, a) => {
+        // a > 0 ? acc.dep += 1  : acc.withdraw += 1
+
+        acc[a > 0 ? 'dep' : 'withdraw'] += a;
+
+        return acc
+    }, {dep: 0, withdraw: 0});
+
+console.log(totalRed)
+
+const text ='work with javascript and react';
+
+const textToTitleCase = function (text) {
+    const exeptions = ['with', 'and', 'to', 'a', ' an'];
+    const titleCase = text
+        .toLowerCase()
+        .split(' ')
+        .map(word => exeptions.includes(word) ? word : word[0].toUpperCase() + word.slice(1))
+        .join(' ');
+    return titleCase
+
+}
+console.log(textToTitleCase(text))
