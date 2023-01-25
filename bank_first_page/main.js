d5889543bdd17ffe7aae7e7e8b79b9c00705ccc9
dimakovalev25 <d.nozdryn@gmail.com>
2023-01-25 20:49:17 +0300

    smooth transition  scrollIntoView

--- bank_first_page/main.js
+++ bank_first_page/main.js
@@ -17,6 +17,9 @@
 
 //.insertAdjacentHTML()
 const mess = document.createElement('div');
+const h1 = document.createElement('h1');
+h1.innerHTML = 'h1 message';
+h1.textContent = 'simple bank';
 // mess.style = 'backgroundColor: red'
 // mess.style.backgroundColor='white';
 // mess.textContent='We use cookies!';
@@ -24,6 +27,7 @@ const mess = document.createElement('div');
 mess.innerHTML = 'We use cookies! <button class="btn btn--close-cookie">OK!</button>';
 const header = document.querySelector('.header');
 header.prepend(mess);
+// header.prepend(h1);
 // header.before(mess);
 // header.after(mess);
 // header.prepend(mess.cloneNode(true));
@@ -34,17 +38,78 @@ document.querySelector('.btn--close-cookie').addEventListener('click', () => mes
 
 //style, class, attribute ----------------------------------------------
 // mess.style.backgroundColor = 'white';
+mess.style.fontSize = '25px'
 // mess.style.width = '100%';
 // console.log(mess.style.width);
 // console.log(getComputedStyle(mess));
-mess.style.height = Number.parseFloat(getComputedStyle(mess).height) + 10 + 'px';
+mess.style.height = Number.parseFloat(getComputedStyle(mess).height) + 15 + 'px';
 // console.log(getComputedStyle(mess).height);
 
 document.documentElement.style.setProperty('--color-first', '#076785')
-// console.log(document.documentElement)
+// console.log(document.documentElement) in css    :root{--color-first: 'red'}
 
+// add class
 mess.classList.add('a');
 mess.classList.remove('a');
 mess.classList.toggle('a');
 mess.classList.contains('a');
 
+
+// events
+
+// const h2 = document.querySelector('h1');
+// h2.addEventListener('mouseenter', function (e) {
+//     console.log('mouse');
+// })
+//
+// const enterH2 = function (e) {
+//     console.log('click')
+//     // h2.removeEventListener('click',enterH2)
+// }
+//
+// setTimeout(() => h2.removeEventListener('click', enterH2), 2000)
+//
+// h2.addEventListener('click', enterH2)
+
+// h2.onmouseenter= function () {  old method
+//     console.log('enter')
+// }
+
+
+//event propagation   rgb(122, 16, 33)
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const getRandomColor = () => `rgb(${getRandomInt(0,255)}, ${getRandomInt(0,255)}, ${getRandomInt(0,255)})`
+
+// console.log(getRandomColor())
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//     console.log('link', e.target)
+//     this.style.backgroundColor = getRandomColor()
+//     e.stopPropagation()
+// })
+//
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//     console.log('links', e.target, e.currentTarget)
+//     this.style.backgroundColor = getRandomColor()
+// })
+//
+// document.querySelector('.nav').addEventListener('click', function (e) {
+//     console.log('nav', e.target, e.currentTarget)
+//     this.style.backgroundColor = getRandomColor()
+// })
+//
+// document.querySelector('body').addEventListener('click', function (e) {
+//     console.log('body', e.target, e.currentTarget)
+//     this.style.backgroundColor = getRandomColor()
+// })
+
+// document.addEventListener('click', function (e) {
+//     console.log('document', e.target)
+//     this.style.backgroundColor = getRandomColor()
+// })
--- bank_first_page/script.js
+++ bank_first_page/script.js
@@ -5,21 +5,31 @@ const btnScroll = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
 btnScroll.addEventListener('click', function (e) {
-
     const section1Coords = section1.getBoundingClientRect();
     console.log(section1Coords)
-
     // window.scrollTo({
     //     left: section1Coords.left + window.pageXOffset,
     //     top: section1Coords.left + window.pageYOffset,
     //     behavior: 'smooth'
     // });
-
     section1.scrollIntoView({
       behavior:'smooth'
     })
+})
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log('links', e.target)
+
+    if(e.target.classList.contains('nav__link')){
+        const href = e.target.getAttribute('href');
+        document.querySelector(href).scrollIntoView({
+            behavior: 'smooth'
+        })
+    }
 
 })
+
 
 
 // Modal window
