51d840548192576584b26b1db9d80101406b9204
dimakovalev25 <d.nozdryn@gmail.com>
2023-01-26 23:02:58 +0300

    lazy loading and slider

--- bank_first_page/script.js
+++ bank_first_page/script.js
@@ -114,17 +114,130 @@ nav.addEventListener('mouseout', function (e) {
 })
 
 
-
 //sticky navigation
 
-const sectionCoords = section1.getBoundingClientRect();
-// console.log(sectionCoords)
+// const sectionCoords = section1.getBoundingClientRect();
+// // console.log(sectionCoords)
+//
+// window.addEventListener('scroll', function (e) {
+//     // console.log(window.scrollY);
+//     if (window.scrollY > sectionCoords.top) {
+//         nav.classList.add('sticky');
+//     } else {
+//         nav.classList.remove('sticky');
+//     }
+// })
 
-window.addEventListener('scroll', function (e) {
-    // console.log(window.scrollY);
-    if (window.scrollY > sectionCoords.top) {
+
+////////////////////////////observer
+//intersection observer api -- sticky
+
+
+const header = document.querySelector('.header');
+const getStickyNav = function (entries) {
+    const entry = entries[0];
+    // console.log(entry.isIntersecting)
+    if (!entry.isIntersecting) {
         nav.classList.add('sticky');
     } else {
         nav.classList.remove('sticky');
     }
+}
+
+const HeaderObserver = new IntersectionObserver(getStickyNav, {
+    root: null,
+    threshold: 0,
+});
+HeaderObserver.observe(header)
+// console.log(observer)
+
+
+//animation parts site
+//section--hidden
+//красивое всплывание секций на сайте
+
+const allSections = document.querySelectorAll('.section');
+const appearanceSection1 = function (entries, observer) {
+
+    const entry = entries[0];
+    if (entry.isIntersecting) entry.target.classList.remove('section--hidden');
+    // observer.unobserve(entry.target)
+
+}
+
+const sectionObserver = new IntersectionObserver(appearanceSection1, {
+    root: null,
+    threshold: 0.25,
+});
+
+allSections.forEach(function (section) {
+    sectionObserver.observe(section);
+    section.classList.add('section--hidden')
+    // console.log(sectionObserver.observe(section))
 })
+
+
+//lazy loading
+
+const lazyImages = document.querySelectorAll('img[data-src]');
+// console.log(lazyImages)
+const loadImages = function (entries, observer) {
+    const entry = entries[0];
+    if (!entry) return
+    entry.target.src = entry.target.dataset.src;
+
+    entry.target.addEventListener('load', function () {
+        entry.target.classList.remove('lazy-img');
+    })
+}
+
+const lazyImagesObserver = new IntersectionObserver(loadImages, {
+    root: null,
+    threshold: 0.7,
+    // rootMargin: '200px'
+})
+
+lazyImages.forEach(image => lazyImagesObserver.observe(image));
+
+
+//Slider
+
+const slider = document.querySelector('.slider');
+// slider.style.transform = 'scale(0.4) translateX(300px)';
+// slider.style.overflow = 'visible';
+
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+
+let currentSlide = 0;
+let slidesNumber = slides.length;
+
+
+btnLeft.addEventListener('click', () => {
+
+    if (currentSlide === 0) {
+        currentSlide = slidesNumber -1;
+    } else {
+        currentSlide--;
+    }
+
+    slides.forEach((slide, i) => slide.style.transform = `translateX(${(i - currentSlide) * 100}%)`)
+    console.log('left')
+})
+
+btnRight.addEventListener('click', () => {
+
+    if (currentSlide === slidesNumber -1) {
+        currentSlide = 0;
+    } else {
+        currentSlide++;
+    }
+
+    slides.forEach((slide, i) => slide.style.transform = `translateX(${(i - currentSlide) * 100}%)`)
+    console.log('right')
+
+})
+
+slides.forEach((slide, i) => slide.style.transform = `translateX(${i * 100}%)`)
+// console.log(slides)
