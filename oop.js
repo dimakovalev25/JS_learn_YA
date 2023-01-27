667cd85120beebf58567e235cd5589ccf027f21f
dimakovalev25 <d.nozdryn@gmail.com>
2023-01-27 20:54:23 +0300

    Prototype Array

--- oop.js
+++ oop.js
@@ -18,13 +18,18 @@ let newarr = [];
 for (const name of arr) {
     const n = new Person(name, 2000);
     newarr.push(n);
-    console.log(newarr)
+    // console.log(newarr)
 }
 // console.log(jo);
+// newarr[0].printAge();
+// console.log(newarr[0].firstName);
 
 const dima = new Person('Dima', 1986);
 const youra = new Person('Youra', 1981);
 
+// console.log(dima.__proto__)
+// console.log(newarr.__proto__)
+// console.log(Array.prototype.__proto__)
 
 // console.log(dima.printAge());
 // console.log(dima.year);
@@ -44,3 +49,24 @@ Person.prototype.personClass = 'Human'
 // console.log(dima.hasOwnProperty('personClass'));
 // console.log(dima.personClass)
 
+
+//Prototype Array
+
+const numbers = [1,2,1];
+// console.log(Object.getPrototypeOf(Object.getPrototypeOf(numbers)))
+
+// console.log(numbers.__proto__)
+// console.log(numbers.__proto__.__proto__)
+// console.log(numbers.__proto__.__proto__.__proto__)
+
+
+//bad practice
+Array.prototype.uniqueElements = function () {
+    return [...new Set(this)]
+}
+
+// console.log(numbers.uniqueElements())
+
+const h2 = document.querySelector('h2');
+// console.dir(h2)
+// console.dir(h => h*1)
