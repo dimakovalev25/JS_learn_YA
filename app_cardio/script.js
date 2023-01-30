ccd5cf507ac54cf39e3d96480df1bcafe63dfae4
dimakovalev25 <d.nozdryn@gmail.com>
2023-01-30 21:16:05 +0300

    https://leafletjs.com/
     use this library

--- app_cardio/script.js
+++ app_cardio/script.js
@@ -8,13 +8,27 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--temp');
 const inputElevation = document.querySelector('.form__input--climb');
 
-if(navigator.geolocation){
+if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
-        function (position){
+        function (position) {
             const {latitude} = position.coords;
             const {longitude} = position.coords;
             console.log(position.coords.latitude, position.coords.longitude)
             console.log(`https://www.google.com/maps/@${latitude},${longitude},14z?hl=ru`)
+
+            const coords = [latitude, longitude];
+
+            const map = L.map('map').setView(coords, 5);
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            L.marker(coords).addTo(map)
+                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+                .openPopup();
+
+
         },
         function () {
             alert('Error! position not found')
@@ -22,3 +36,4 @@ if(navigator.geolocation){
 
         })
 }
+
