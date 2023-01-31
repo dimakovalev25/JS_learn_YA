3be7fe69c1724fbbca558638351ab016bfaf002f
dimakovalev25 <d.nozdryn@gmail.com>
2023-01-31 20:29:55 +0300

    callback hell

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -3,26 +3,16 @@
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
-const getCountryData = function (country) {
-    const request = new XMLHttpRequest();
-    request.open('GET', `https://restcountries.com/v3.1/name/${country}`)
-    request.send();
 
-    request.addEventListener('load', function () {
-        const [data] = JSON.parse(this.responseText)
-        // console.log(this.responseText)
-        console.log(data)
+const dispayCountry = function (data, className = '') {
 
-        const currencies = data.currencies;
-        const currenciesName = Object.values(currencies)[0];
-        console.log(currenciesName)
+    const currencies = data.currencies;
+    const currenciesName = Object.values(currencies)[0];
+    const languages = data.languages;
+    const languagesName = Object.values(languages)[0];
 
-        const languages = data.languages;
-        const languagesName = Object.values(languages)[0];
-        console.log(languagesName)
-
-        const html = `        
-        <article class="country">
+    const html = `        
+        <article class="country ${className}">
           <img class="country__img" src="${data.flags.svg}" />
           <div class="country__data">
             <h3 class="country__name">${data.name.common}</h3>
@@ -33,14 +23,52 @@ const getCountryData = function (country) {
           </div>
         </article>`
 
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+}
+const getCountryData = function (country) {
 
-        countriesContainer.insertAdjacentHTML('beforeend', html);
-        countriesContainer.style.opacity = 1;
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.com/v3.1/name/${country}`)
+    request.send();
+    request.addEventListener('load', function () {
+        const [data] = JSON.parse(this.responseText)
+
+
+        dispayCountry(data);
+
+        const [firstNeighbor] = data.borders;
+        console.log(firstNeighbor);
+        if (!firstNeighbor) return;
+
+        const request2 = new XMLHttpRequest();
+        request2.open('GET', `https://restcountries.com/v3.1/alpha/${firstNeighbor}`)
+        request2.send();
+        request2.addEventListener('load', function () {
+            const [data2] = JSON.parse(this.responseText);
+
+            console.log(data2);
+            dispayCountry(data2, 'neighbour');
+
+
+        })
     })
 }
 
-getCountryData('belarus');
-getCountryData('russia');
-getCountryData('argentina');
-// getCountryData('usa');
+// getCountryData('belarus');
+// getCountryData('russia');
+// getCountryData('argentina');
+getCountryData('usa');
 
+setTimeout(() => {
+    console.log(1);
+    setTimeout(() => {
+        console.log(2)
+        setTimeout(() => {
+            console.log(3)
+            setTimeout(() => {
+                console.log(4)
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}, 1000)
